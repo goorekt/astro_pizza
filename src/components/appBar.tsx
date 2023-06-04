6e3c6fd93a136682156fc734e9f2ac019910f3e4
goorekt <asadiali008@gmail.com>
2023-06-04 15:34:13 +0200

    d

---
+++ src/components/appBar.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { AiOutlinePhone } from "react-icons/ai";
+import { AiFillHome } from "react-icons/ai";
+import { IoMdMenu } from "react-icons/io";
+interface Props {
+	className?: string;
+}
+
+const options = ["Home", "Contact", "Menu"];
+
+const AppBar = ({ className }: Props) => {
+	return (
+		<div
+			className={twMerge(
+				"flex  h-24 w-screen flex-row items-center justify-between bg-slate-100 px-5 text-lg shadow-lg md:h-32 ",
+				className
+			)}
+		>
+			<AiFillHome size={50} className="scale-75 cursor-pointer md:scale-100 " />
+
+			<div className="flex min-w-min flex-row justify-between  gap-3 sm:text-xs  md:text-2xl">
+				
+					<ul className="hidden flex-row gap-3 md:flex  md:gap-16 " id="navbar-default">
+						{options.map((element) => (
+							<li>
+								<a
+									href="#"
+									className="transition-scale flex cursor-pointer items-center duration-200 hover:scale-105"
+								>
+									{element}
+								</a>
+							</li>
+						))}
+					</ul>
+			
+				
+
+       
+			</div>
+		</div>
+	);
+};
+
+export default AppBar;
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(-3deg)" },
+					"50%": { transform: "rotate(3deg)" },
+				},
+			},
+			animation: {
+				wiggle: "wiggle 0.3s ease-in-out infinite",
+				'spin-slow': 'spin 30s linear infinite',
+			},
+		},
+	},
+	plugins: [],
+};
